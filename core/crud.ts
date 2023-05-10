dd45548be18f28c328247531b261f249d95ffd5d
Padilha1 <padilha.matheus@hotmail.com>
2023-05-09 22:04:06 -0300

    Colocado Typescript

--- core/crud.ts
+++ core/crud.ts
@@ -1,11 +1,12 @@
-const fs = require("fs");
+import fs from "fs";  //ES6
+// const fs = require("fs"); -- CommonJS
 
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
 
-function create (content) {
+function create (content: string) {
     //salvar content no sistema
     fs.writeFileSync(DB_FILE_PATH, content)  
 
