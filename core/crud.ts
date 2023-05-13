807dcff9d86468c78d03ddf67f67b6b70de79ccf
Padilha1 <padilha.matheus@hotmail.com>
2023-05-13 15:11:07 -0300

    Aula READ CRUD - clear, read, Interface

--- core/crud.ts
+++ core/crud.ts
@@ -1,17 +1,54 @@
-import fs from "fs";  //ES6
+import fs from "fs"; //ES6
 // const fs = require("fs"); -- CommonJS
 
 const DB_FILE_PATH = "./core/db";
 
 console.log("[CRUD]");
 
-
-function create (content: string) {
-    //salvar content no sistema
-    fs.writeFileSync(DB_FILE_PATH, content)  
-
-    return content;
+interface Todo {
+	date: string;
+	content: string;
+	done: boolean;
 }
 
+function create(content: string) {
+	const todo: Todo = {
+		date: new Date().toISOString(),
+		content: content,
+		done: false,
+	};
+	const todos: Array<Todo> = [...read(), todo];
 
-create("Today we need to do...")
+	//salvar content no sistema
+	fs.writeFileSync(
+		DB_FILE_PATH,
+		JSON.stringify(
+			{
+				todos,
+			},
+			null,
+			2
+		)
+	);
+
+	return content;
+}
+
+function read(): Array<Todo> {
+	const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
+	const db = JSON.parse(dbString || "{}");
+	if (!db.todos) {
+		return [];
+	}
+	return db.todos;
+}
+
+function CLEAR_DB() {
+	fs.writeFileSync(DB_FILE_PATH, "");
+}
+
+//Simulation
+CLEAR_DB();
+create("Primeira Todo!");
+create("Segunda TODO!");
+console.log(read());
